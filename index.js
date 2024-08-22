17273c80d7528e0989cb34ff2ab3b34342ab930d
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2024-08-22 05:16:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  // origin: 'https://dripz-frontend.vercel.app', 
-  origin: 'http://localhost:3000', 
+  origin: 'https://dripz-frontend.vercel.app', 
+  // origin: 'http://localhost:3000', 
   credentials: true // Allow credentials
 }));
 const apiLimiter = rateLimit({
